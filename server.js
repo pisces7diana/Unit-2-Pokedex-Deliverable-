1c023b475873c7d2d2f8041d635206803ad974ea
pisces7diana <email7diana@gmail.com>
2023-12-25 11:21:41 -0800

    Server is working and displays a plain index page

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * DEPENDENCIES
+ */
+// import the express library
+const express = require('express')
+const PORT= 3000
+// import the pokemon.js
+const pokemon = require('./models/pokemon.js')
+/**
+ * APPLICATION OBJECT - This is the center of our express universe
+ */
+const app = express()
+/**
+ * MIDDLEWARE
+ * Special Utilities that run before our routes
+ */
+/**
+ * ROUTES - INDUCES
+ */
+/**
+ * Index
+ */
+app.get('/pokemon', (req, res) => {
+    // res.send(pokemon)
+    // res.render('index.ejs', {pokemon})
+})
+/**
+ * SERVER LISTENER - tells our app to listen for req on a certain port
+ */
+app.listen(3000, () => {
+    console.log(`server is listening on ${PORT}`)
+})
